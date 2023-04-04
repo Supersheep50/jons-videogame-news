1377d34c4d88789c471933c9307dc1cf97c37d3b
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-04-04 13:29:11 +0000

    Add login and logout feature and styling

--- JVN/settings.py
+++ JVN/settings.py
@@ -34,6 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["JVN.herokuapp.com", "localhost"]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Application definition
 
@@ -43,12 +45,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'NEWS',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
