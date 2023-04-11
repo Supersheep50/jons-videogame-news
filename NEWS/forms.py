4aca533527c96353afc9008a08a77d4f415cc018
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-04-11 09:01:18 +0000

    Add comments to python code

--- NEWS/forms.py
+++ NEWS/forms.py
@@ -1,7 +1,7 @@
 from .models import Comment
 from django import forms
 
-
+#Comment form
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
--- NEWS/views.py
+++ NEWS/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
-
+#News Artcile view
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -66,7 +66,7 @@ class PostDetail(View):
             },
         )
 
-
+#Code for Like functionality
 class PostLike(View):
     
     def post(self, request, slug, *args, **kwargs):
