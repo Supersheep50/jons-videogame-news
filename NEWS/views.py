560c59f13222656dcacc50179e75f097d14a8366
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-09-16 10:17:17 +0000

    Make changes to views.py to ensure better security

--- NEWS/views.py
+++ NEWS/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
-from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.generic import (TemplateView)
+from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm
 
-# News Article view (credit to course material in Readme)
+
+# News Artcile view (credit to course material in Readme)
+
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
 
+
 class PostDetail(View):
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -34,6 +39,7 @@ class PostDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -43,7 +49,7 @@ class PostDetail(View):
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            comment_form.instance.email = request.user email
+            comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
@@ -64,6 +70,8 @@ class PostDetail(View):
         )
 
 # Code to Edit comments (credit to course material in Readme)
+
+
 class EditCommentView(View):
     def get(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
@@ -87,7 +95,7 @@ class EditCommentView(View):
                 "comments": comment.post.comments.filter(approved=True).order_by("-created_on"),
                 "commented": True,
                 "comment_form": comment_form,
-                "liked": comment post.likes.filter(id=request.user.id).exists(),
+                "liked": comment.post.likes.filter(id=request.user.id).exists(),
             }
             return render(request, "article.html", context)
         else:
@@ -96,7 +104,10 @@ class EditCommentView(View):
         context = {'form': comment_form, 'commented': False}  
         return render(request, 'edit_item.html', context)
 
-# Code to Delete comments (credit to tutor support and course material in Readme)
+
+# Code to Edit comments (credit to tutor support and course material in Readme)
+
+
 class DeleteCommentView(View):
     def get(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
@@ -109,8 +120,12 @@ class DeleteCommentView(View):
         context = {'deleted': True}  
         return render(request, 'delete_item.html', context)
 
+        
 # Code for Like functionality
+
+
 class PostLike(View):
+    
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
@@ -120,8 +135,14 @@ class PostLike(View):
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
+
 class AboutPage(TemplateView):
+
     template_name = 'about.html'
 
+
 class ContactPage(TemplateView):
+
     template_name = 'contact.html'
+
+
