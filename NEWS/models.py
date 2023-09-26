d1ef59420b3d2198ecfce3ae2694f53527f88788
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-09-26 16:33:52 +0000

    Add author field to comment model

--- NEWS/models.py
+++ NEWS/models.py
@@ -38,6 +38,9 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
