7ca5c37e799f2104751633a47ac0e2c940823407
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-12-07 17:59:34 +0000

    made more changes for pep8

--- NEWS/views.py
+++ NEWS/views.py
@@ -21,25 +21,26 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = post.likes.filter(id=request.user.id).exists()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-        return render(
-            request,
-            "article.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": False,
-                "liked": liked,
-                "comment_form": CommentForm()
-            },
-        )
+        context = {
+            "post": post,
+            "comments": comments,
+            "commented": False,
+            "liked": liked,
+            "comment_form": CommentForm()
+        }
+        return render(request, "article.html", context)
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = post.likes.filter(id=request.user.id).exists()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -51,17 +52,16 @@ class PostDetail(View):
         else:
             comment_form = CommentForm()
 
-        return render(
-            request,
-            "article.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
-                "liked": liked
-            },
-        )
+        context = {
+            "post": post,
+            "comments": comments,
+            "commented": True,
+            "comment_form": comment_form,
+            "liked": liked
+        }
+        return render(request, "article.html", context)
+
+# Code to Edit comments (credit to course material in Readme)
 
 
 class EditCommentView(View):
@@ -71,9 +71,15 @@ class EditCommentView(View):
             return HttpResponseForbidden(
                 "You don't have permission to edit this comment."
             )
-
-        form = CommentForm(instance=comment)
-        return render(request, 'edit_item.html', {'form': form, 'commented': False})
+    form = CommentForm(instance=comment)
+    return render(
+        request,
+        'edit_item.html',
+        {
+            'form': form,
+            'commented': False
+        }
+    )
 
     def post(self, request, comment_id, *args, **kwargs):
         comment = get_object_or_404(Comment, id=comment_id)
@@ -84,19 +90,26 @@ class EditCommentView(View):
 
         comment_form = CommentForm(data=request.POST, instance=comment)
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.save()
-
+            comment_form.save()
             context = {
                 "post": comment.post,
-                "comments": comment.post.comments.filter(approved=True).order_by("-created_on"),
+                "comments": comment.post.comments.filter
+                (approved=True).order_by(
+                    "-created_on"
+                ),
                 "commented": True,
                 "comment_form": CommentForm(),
-                "liked": comment.post.likes.filter(id=request.user.id).exists(),
+                "liked": comment.post.likes.filter(id=request.user.id).exists()
             }
             return render(request, "article.html", context)
         else:
-            return render(request, 'edit_item.html', {'form': comment_form, 'commented': False})
+            return render(
+                request,
+                'edit_item.html',
+                {'form': comment_form, 'commented': False}
+            )
+
+# Code for Delete Comment functionality
 
 
 class DeleteCommentView(View):
@@ -107,7 +120,8 @@ class DeleteCommentView(View):
                 "You don't have permission to delete this comment."
             )
 
-        return render(request, 'delete_item.html', {'comment': comment})
+        context = {'comment': comment}
+        return render(request, 'delete_item.html', context)
 
     def post(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
@@ -120,8 +134,11 @@ class DeleteCommentView(View):
         return redirect('your-success-page-url')
 
 
+# Code for Like functionality
+
+
 class PostLike(View):
-    
+
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
@@ -133,8 +150,10 @@ class PostLike(View):
 
 
 class AboutPage(TemplateView):
+
     template_name = 'about.html'
 
 
 class ContactPage(TemplateView):
+
     template_name = 'contact.html'
