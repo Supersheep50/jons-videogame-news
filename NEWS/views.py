4370b06a3edcd50589a7492a150cdca9490a9569
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-12-07 17:41:09 +0000

    made changes to view.py for pep8 linter

--- NEWS/views.py
+++ NEWS/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
-from django.views.generic import (TemplateView)
-from django.http import HttpResponseRedirect
-from django.http import HttpResponseForbidden
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from .models import Post, Comment
 from .forms import CommentForm
 
-
-# News Artcile view (credit to course material in Readme)
+# News Article view (credit to course material in Readme)
 
 
 class PostList(generic.ListView):
@@ -23,9 +21,7 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        liked = post.likes.filter(id=request.user.id).exists()
 
         return render(
             request,
@@ -40,13 +36,10 @@ class PostDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
-
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        liked = post.likes.filter(id=request.user.id).exists()
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -70,29 +63,30 @@ class PostDetail(View):
             },
         )
 
-# Code to Edit comments (credit to course material in Readme)
-
 
 class EditCommentView(View):
     def get(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
         if comment.author != request.user and not request.user.is_superuser:
-            return HttpResponseForbidden("You don't have permission to edit this comment.")
-        
+            return HttpResponseForbidden(
+                "You don't have permission to edit this comment."
+            )
+
         form = CommentForm(instance=comment)
-        context = {'form': form, 'commented': False}  
-        return render(request, 'edit_item.html', context)
+        return render(request, 'edit_item.html', {'form': form, 'commented': False})
 
     def post(self, request, comment_id, *args, **kwargs):
         comment = get_object_or_404(Comment, id=comment_id)
         if comment.author != request.user and not request.user.is_superuser:
-            return HttpResponseForbidden("You don't have permission to edit this comment.")
+            return HttpResponseForbidden(
+                "You don't have permission to edit this comment."
+            )
 
         comment_form = CommentForm(data=request.POST, instance=comment)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.save()
-            
+
             context = {
                 "post": comment.post,
                 "comments": comment.post.comments.filter(approved=True).order_by("-created_on"),
@@ -102,32 +96,28 @@ class EditCommentView(View):
             }
             return render(request, "article.html", context)
         else:
-            context = {'form': comment_form, 'commented': False}  
-            return render(request, 'edit_item.html', context)
-
-
-# Code to Edit comments (credit to tutor support and course material in Readme)
+            return render(request, 'edit_item.html', {'form': comment_form, 'commented': False})
 
 
 class DeleteCommentView(View):
     def get(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
         if comment.author != request.user and not request.user.is_superuser:
-            return HttpResponseForbidden("You don't have permission to delete this comment.")
+            return HttpResponseForbidden(
+                "You don't have permission to delete this comment."
+            )
 
-        context = {'comment': comment}
-        return render(request, 'delete_item.html', context)
+        return render(request, 'delete_item.html', {'comment': comment})
 
     def post(self, request, comment_id):
         comment = get_object_or_404(Comment, id=comment_id)
         if comment.author != request.user and not request.user.is_superuser:
-            return HttpResponseForbidden("You don't have permission to delete this comment.")
+            return HttpResponseForbidden(
+                "You don't have permission to delete this comment."
+            )
 
         comment.delete()
         return redirect('your-success-page-url')
-
-        
-# Code for Like functionality
 
 
 class PostLike(View):
@@ -143,12 +133,8 @@ class PostLike(View):
 
 
 class AboutPage(TemplateView):
-
     template_name = 'about.html'
 
 
 class ContactPage(TemplateView):
-
     template_name = 'contact.html'
-
-
